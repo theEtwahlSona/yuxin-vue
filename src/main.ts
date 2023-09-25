daaa6a98b75bbee1f4dd67214d1cbc95d2e73057
etwahl <1404483052@qq.com>
2023-09-25 23:06:02 +0800

    feat: logger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import './style.css'
 import App from './App.vue'
+import logger from "@/utils/logger";
 
 createApp(App).mount('#app')
+
+logger.prettyPrimary(`欢迎使用`, import.meta.env.VITE_APP_TITLE)
--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,100 +1,122 @@
 const isArray = function (obj: any): boolean {
-  return Object.prototype.toString.call(obj) === '[object Array]'
+    return Object.prototype.toString.call(obj) === '[object Array]'
 }
 
-const Logger = () => {}
-
-Logger.typeColor = function (type: string) {
-  let color = ''
-  switch (type) {
-    case 'primary':
-      color = '#2d8cf0'
-      break
-    case 'success':
-      color = '#19be6b'
-      break
-    case 'info':
-      color = '#909399'
-      break
-    case 'warn':
-      color = '#ff9900'
-      break
-    case 'error':
-      color = '#f03f14'
-      break
-    default:
-      color = '#35495E'
-      break
-  }
-  return color
+const typeColor = (type: string): string => {
+    let color = ''
+    switch (type) {
+        case 'primary':
+            color = '#2d8cf0'
+            break
+        case 'success':
+            color = '#19be6b'
+            break
+        case 'info':
+            color = '#909399'
+            break
+        case 'warn':
+            color = '#ff9900'
+            break
+        case 'error':
+            color = '#f03f14'
+            break
+        default:
+            color = '#35495E'
+            break
+    }
+    return color
 }
 
-Logger.print = function (type = 'default', text: any, back = false) {
-  if (typeof text === 'object') {
-    // 如果是對象則調用打印對象方式
-    isArray(text) ? console.table(text) : console.dir(text)
-    return
-  }
-  if (back) {
-    // 如果是打印帶背景圖的
+const print = (type: string = 'default', text: any, back: boolean = false): void => {
+    if (typeof text === 'object') {
+        // 如果是对象则调用打印对象方式
+        isArray(text) ? console.table(text) : console.dir(text)
+        return
+    }
+    if (back) {
+        // 如果是打印带背景图的
+        console.log(
+            `%c ${text} `,
+            `background:${typeColor(type)}; 
+             padding: 2px; 
+             border-radius: 4px; 
+             color: #fff;`
+        )
+    } else {
+        console.log(
+            `%c ${text} `,
+            `border: 1px solid ${typeColor(type)};
+             padding: 2px; 
+             border-radius: 4px;
+             color: ${typeColor(type)};`
+        )
+    }
+}
+
+const printBack = (type: string = 'primary', text: any): void => {
+    print(type, text, true)
+}
+
+const pretty = (type: string = 'primary', title: string, text: any): void => {
+    if (typeof text === 'object') {
+        console.group('Console Group', title)
+        console.log(
+            `%c ${title}`,
+            `background:${typeColor(type)};
+             border:1px solid ${typeColor(type)};
+             padding: 1px; 
+             border-radius: 4px; 
+             color: #fff;`
+        )
+        isArray(text) ? console.table(text) : console.dir(text)
+        console.groupEnd()
+        return
+    }
+
     console.log(
-      `%c ${text} `,
-      `background:${Logger.typeColor(type)}; padding: 2px; border-radius: 4px; color: #fff;`
+        `%c ${title} %c ${text} %c`,
+        `background:${typeColor(type)};
+        border:1px solid ${typeColor(type)};
+        padding: 1px; 
+        border-radius: 4px 0 0 4px; 
+        color: #fff;`,
+        `border:1px solid ${typeColor(type)};
+         padding: 1px; 
+         border-radius: 0 4px 4px 0;
+         color: ${typeColor(type)};`,
+        'background:transparent'
     )
-  } else {
-    console.log(
-      `%c ${text} `,
-      `border: 1px solid ${Logger.typeColor(type)};
-        padding: 2px; border-radius: 4px;
-        color: ${Logger.typeColor(type)};`
-    )
-  }
 }
 
-Logger.printBack = function (type = 'primary', text) {
-  this.print(type, text, true)
+const prettyPrimary = (title: string, ...text: any): void => {
+    text.forEach((t) => pretty('primary', title, t))
 }
 
-Logger.pretty = function (type = 'primary', title, text) {
-  if (typeof text === 'object') {
-    console.group('Console Group', title)
-    console.log(
-      `%c ${title}`,
-      `background:${Logger.typeColor(type)};border:1px solid ${Logger.typeColor(type)};
-        padding: 1px; border-radius: 4px; color: #fff;`
-    )
-    isArray(text) ? console.table(text) : console.dir(text)
-    console.groupEnd()
-    return
-  }
-  console.log(
-    `%c ${title} %c ${text} %c`,
-    `background:${Logger.typeColor(type)};border:1px solid ${Logger.typeColor(type)};
-      padding: 1px; border-radius: 4px 0 0 4px; color: #fff;`,
-    `border:1px solid ${Logger.typeColor(type)};
-      padding: 1px; border-radius: 0 4px 4px 0; color: ${Logger.typeColor(type)};`,
-    'background:transparent'
-  )
+const prettySuccess = (title: string, ...text: any[]): void => {
+    text.forEach((t) => pretty('success', title, t))
 }
 
-Logger.prettyPrimary = function (title, ...text) {
-  text.forEach((t) => this.pretty('primary', title, t))
+const prettyWarn = (title: string, ...text: any[]): void => {
+    text.forEach((t) => pretty('warn', title, t))
 }
 
-Logger.prettySuccess = function (title, ...text) {
-  text.forEach((t) => this.pretty('success', title, t))
+const prettyError = (title: string, ...text: any[]): void => {
+    text.forEach((t) => pretty('error', title, t))
 }
 
-Logger.prettyWarn = function (title, ...text) {
-  text.forEach((t) => this.pretty('warn', title, t))
+const prettyInfo = (title: string, ...text: any[]): void => {
+    text.forEach((t) => pretty('info', title, t))
 }
 
-Logger.prettyError = function (title, ...text) {
-  text.forEach((t) => this.pretty('error', title, t))
+const logger = {
+    typeColor,
+    printBack,
+    pretty,
+    prettyPrimary,
+    prettySuccess,
+    prettyWarn,
+    prettyError,
+    prettyInfo
 }
 
-Logger.prettyInfo = function (title, ...text) {
-  text.forEach((t) => this.pretty('info', title, t))
-}
-
-export default Logger
+export default logger
